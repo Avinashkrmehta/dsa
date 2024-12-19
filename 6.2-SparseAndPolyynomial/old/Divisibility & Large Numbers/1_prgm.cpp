e3ff83ac22c646b112d2e58bec4c86ad2cf686aa
Avinashkrmehta <avi4791@gmail.com>
2024-12-20 04:56:57 +0530

    rearrang the files

---
+++ 6.2-SparseAndPolyynomial/old/Divisibility & Large Numbers/1_prgm.cpp
@@ -0,0 +1,15 @@
+// Check if a large number is divisible by 3 or not
+#include<bits/stdc++.h>
+using namespace std;
+
+int check(string str){
+  int len = str.length();
+  int digitSum = 0;
+
+}
+
+int main(){
+  string str = "123456";
+  cout << '1' + '0';
+  return 0;
+}
