74175282cb4c7d60d8e8a702b124886fab105aef
Avinashkrmehta <avi4791@gmail.com>
2025-01-13 05:11:20 +0530

    array abstract data type

---
+++ 6.0-Array-ADT/README.md
@@ -0,0 +1,16 @@
+## Array ADT
+
+<img src="./images/imagea.png" width="500" />
+
+## Inserting in an Array
+### Display
+<img src="./images/imageb.png" width="500" />
+
+#### Add & Append
+<img src="./images/imagec.png" width="500" />
+
+#### Insert
+<img src="./images/imaged.png" width="500" />
+
+#### Delete
+<img src="./images/imagee.png" width="500" />
---
+++ 6.0-Array-ADT/programs/cpp/program2.cpp
@@ -0,0 +1,47 @@
+// Append and Insert
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Array{
+  int A[10];
+  int size;
+  int length;
+};
+
+void Display(struct Array arr){
+  cout << "Elements are: ";
+  for(int i = 0; i < arr.length; i++){
+    cout << arr.A[i] << " ";
+  }
+}
+
+void Append(struct Array *arr, int x){
+  if(arr->length < arr->size){
+    arr->A[arr->length++] = x;
+  }
+}
+
+void Insert(struct Array *arr, int index, int x){
+  if(index >= 0 && index <= arr->length){
+    for(int i = arr->length; i > index; i--){
+      arr->A[i] = arr->A[i-1];
+    }
+    arr->A[index] = x;
+    arr->length++;
+  }
+}
+
+
+int main(){
+  struct Array arr = {{2,3,4,5,6}, 10, 5};
+  Display(arr);
+  cout << endl;
+  Append(&arr, 10);
+  Display(arr);
+  cout << endl;
+  Insert(&arr, 2, 12);
+  Display(arr);
+
+  return 0;
+}
