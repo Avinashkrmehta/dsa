824e2a76572af932e3beede6702db261814444ae
AKMehta <avi4791@gmail.com>
2025-01-26 03:45:28 +0530

    Matrix (#5)
    * Matrix-day one
    
    * matrix changes
    
    * matrix other types matrices

---
+++ 6.1-Martix/programs/cpp/program7.cpp
@@ -0,0 +1,16 @@
+// Menu Driven Program to perform Matrix Operations
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void main(){
+    int *A, n, ch,x;
+    cout << "Enter dimension of matrix: ";
+    cin >> n;
+    A = new int[n];  
+    
+    
+
+}
